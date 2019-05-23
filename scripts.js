1c2a0bb937966387b26e018d43564fc57087c028
Alice-Lau <alice.p.k.lau@gmail.com>
2019-05-23 16:13:41 -0400

    added gitignore & added select city event listener

--- scripts.js
+++ scripts.js
@@ -1,17 +1,38 @@
+const restaurantApp = {}
+
+const $selectedCity = $('#citySelection');
+
+
+$selectedCity.on('change', function() {
+    restaurantApp.getInput();
+})
+
+restaurantApp.getInput = function() {
+    const entityId = parseInt($selectedCity.val());
+
+    if(entityId === NaN) {
+        console.log('error message');
+    } else {
+        restaurant.ajaxRequest(entityId);
+    }
+}
+
 
 
 
 
 // ajax call to zomato
-$(function () {
+restaurantApp.ajaxRequest = function(entityId, cuisinesType) {
     $.ajax({
         url: "https://developers.zomato.com/api/v2.1/search?",
         type: "GET",
         dataType: "JSON",
         data: {
-            count: 10,
+            count: 12,
             sort: 'rating',
-            order: 'desc'
+            entity_id: entityId,
+            cuisines: cuisinesType
+
         },
         beforeSend: function (xhr) {
             xhr.setRequestHeader("user-key", "b7d63a85e5a9127cf51fb71ccf4c92e6");
@@ -21,9 +42,9 @@ $(function () {
             // console.log(restaurantArray);
 
             for (restauranObj of restaurantArray) {
-                
+
                 const restaurant = restauranObj.restaurant;
-                
+
                 const { name, location, cuisines, price_range, user_rating, featured_image } = restaurant;
 
                 const address = location.address;
@@ -41,4 +62,13 @@ $(function () {
             }
         },
     });
-});
+}
+
+
+//doc ready
+$('document').ready(function() {
+
+
+
+
+})
